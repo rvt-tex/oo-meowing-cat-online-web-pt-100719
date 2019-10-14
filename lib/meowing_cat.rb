9b3dfa92f5b7d2778afbab2eb89f7c37ba4c3e72
Ruel Tingling <rvtingling@gmail.com>
2019-10-14 13:43:14 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -8,4 +8,5 @@ class Cat
 end 
 
 # line 2 we are defining the cat class
-# line 3 we are using macros 
+# line 3 we are using macros (attr_accessor) to create the setter and getter methods for the cats name.
+# line 5 we are defining the sound of the cat, the syntax puts is used to out put the sound when the method is called on an instance of a cat.
